b78c7e2393cbfdbd89e7a3cdd7406e58014b36e8
lajsf <lajsf@lajsfdeMacBook-Pro.local>
2024-01-10 17:56:00 +0800

    create-project 删除无用代码

--- src/components/create-project/index.tsx
+++ src/components/create-project/index.tsx
@@ -29,13 +29,7 @@ const CreateProjectModule:React.FC<CreateProjectModuleProps> = ({isOpen, onClose
             className="home-login-modal create-project">
                 <div className="title">小说别名（必填）</div>
                 <Input placeholder="请输入小说别名"  size="large"  onChange={(v)=> setName(v.target.value)}/>
-                <div className="title flexR">Stable-Diffusion-WebUI环境<span>已启动</span></div>
-                <div className=" flexR">
-                    <Input placeholder="http://127.0.0.1:7860/"  size="large" />
-                    <div className="btn-refresh"><SyncOutlined /></div>
-                </div>
-                <div className="sub-text flexR">请保证SDWebUI启动，并开启AP服务<span>开启方法</span></div>
-                <Button type="primary" block className="btn-primary-auto" onClick={handleCreate}>创建项目</Button>
+                <Button type="primary" block className="btn-primary-auto" style={{marginTop: '20px'}} onClick={handleCreate}>创建项目</Button>
         </Modal>
     )
 }
