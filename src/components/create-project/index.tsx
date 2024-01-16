3124826feebe6ceb6cbf2af4f0f1caa6c3a7d4f6
lajsf <lajsf@lajsfdeMacBook-Pro.local>
2024-01-16 17:29:43 +0800

    创建项目modal

--- src/components/create-project/index.tsx
+++ src/components/create-project/index.tsx
@@ -3,23 +3,27 @@ import { useState } from "react";
 import { history } from 'umi'
 import "./index.less"
 import { usePersistWorkspaces } from "@/stores/project";
+import { ProjectType } from "@/pages/index";
 
 interface ProjectProps {
     isOpen: boolean,
-    onClose: () => void
+    onClose: () => void,
+    type: ProjectType
 }
 
-export const StoryProjectModal: React.FC<ProjectProps> = ({ isOpen, onClose }) => {
+export const ProjectModal: React.FC<ProjectProps> = ({ isOpen, onClose, type }) => {
     const [name, setName] = useState("")
-
-
     const { create } = usePersistWorkspaces(state => state)
 
     const handleCreate = () => {
         if (!name) {
             return message.error("请输入小说别名");
         }
-        create("story", name).then((id) => { history.push("/story/" + id) })
+        if(type === 'story') {
+            create("story", name).then((id) => { history.push("/story/" + id) })
+        }else {
+            create("imitate", name).then((id) => { history.push("/imitate/" + id) })
+        }
     }
 
     return (
@@ -31,31 +35,6 @@ export const StoryProjectModal: React.FC<ProjectProps> = ({ isOpen, onClose }) =
             className="home-login-modal create-project">
             <div className="title">小说别名（必填）</div>
             <Input placeholder="请输入小说别名" size="large" onChange={(v) => setName(v.target.value)} />
-            <Button type="primary" block className="btn-primary-auto" style={{ marginTop: '20px' }} onClick={handleCreate}>创建项目</Button>
-        </Modal>
-    )
-}
-
-export const ImitateProjectModal: React.FC<ProjectProps> = ({ isOpen, onClose }) => {
-    const [name, setName] = useState("")
-    const { create } = usePersistWorkspaces(state => state)
-    const handleCreate = () => {
-        if (!name) {
-            return message.error("请输入项目名");
-        }
-        // 缺少校验
-        create("imitate", name).then((id) => { history.push("/imitate/" + id) })
-    }
-
-    return (
-        <Modal title="创建项目"
-            open={isOpen}
-            onCancel={onClose}
-            footer={null}
-            width={700}
-            className="home-login-modal create-project">
-            <div className="title">项目名（必填）</div>
-            <Input placeholder="请输入项目别名" size="large" onChange={(v) => setName(v.target.value)} />
             <Button type="primary" block className="btn-primary-auto" style={{ marginTop: '20px' }} onClick={handleCreate}>创建项目</Button>
         </Modal>
     )
